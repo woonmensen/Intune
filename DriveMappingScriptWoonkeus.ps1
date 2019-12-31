6e2cbb603dd99fa878af8362b55257fc9b0a8665
woonmensen <59387573+woonmensen@users.noreply.github.com>
2019-12-31 13:23:59 +0100

    DriveMappingScriptWoonkeus.ps1
    Login script voor Woonkeus

---
+++ DriveMappingScriptWoonkeus.ps1
@@ -0,0 +1,68 @@
+Start-Transcript -Path $(Join-Path $env:temp "DriveMapping.log")
+
+$driveMappingConfig=@()
+
+######################################################################
+#                section script configuration                        #
+######################################################################
+
+<#
+   Add your internal Active Directory Domain name and custom network drives below
+#>
+
+$dnsDomainName= "wm.local"
+
+$driveMappingConfig+=  [PSCUSTOMOBJECT]@{
+    DriveLetter = "S"
+    UNCPath= "\\fs02\scans"
+    Description="Scans"
+}
+
+$driveMappingConfig+=  [PSCUSTOMOBJECT]@{
+    DriveLetter = "U"
+    UNCPath= "\\fs02\Woonkeus"
+    Description="Woonkeus"
+}
+
+######################################################################
+#               end section script configuration                     #
+######################################################################
+
+$connected=$false
+$retries=0
+$maxRetries=3
+
+Write-Output "Starting script..."
+do {
+    
+    if (Resolve-DnsName $dnsDomainName -ErrorAction SilentlyContinue){
+    
+        $connected=$true
+
+    } else{
+ 
+        $retries++
+        
+        Write-Warning "Cannot resolve: $dnsDomainName, assuming no connection to fileserver"
+ 
+        Start-Sleep -Seconds 3
+ 
+        if ($retries -eq $maxRetries){
+            
+            Throw "Exceeded maximum numbers of retries ($maxRetries) to resolve dns name ($dnsDomainName)"
+        }
+    }
+ 
+}while( -not ($Connected))
+
+#Map drives
+    $driveMappingConfig.GetEnumerator() | ForEach-Object {
+
+        Write-Output "Mapping network drive $($PSItem.UNCPath)"
+
+        New-PSDrive -PSProvider FileSystem -Name $PSItem.DriveLetter -Root $PSItem.UNCPath -Description $PSItem.Description -Persist -Scope global
+
+        (New-Object -ComObject Shell.Application).NameSpace("$($PSItem.DriveLetter):").Self.Name=$PSItem.Description
+}
+
+Stop-Transcript

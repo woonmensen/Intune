1e65b5b1452fac3d92f246e08edbcd0f4269d3a3
woonmensen <59387573+woonmensen@users.noreply.github.com>
2020-08-26 14:34:44 +0200

    Add files via upload
    Office sjablonen kopieren

--- LogonScript.ps1
+++ LogonScript.ps1
@@ -21,16 +21,23 @@ Remove-Item "$env:userprofile\Desktop\Microsoft Edge.lnk" -ErrorAction Ignore
 # Maak een default handtekening in Outlook, indien deze nog niet bestaat.
 # Niet overschrijven, de gebruiker zal deze aangepast hebben.
 $s = "$basedir\Outlook\woonmensen.htm"
-#$t = "$ENV:APPDATA\Microsoft\Handtekeningen\woonmensen.htm"
-#if (-not (Test-Path "$t")){
-#	write-host "Nieuwe handtekening: $t"
-#	copy "$s" "$t"
-#}
 $t = "$ENV:APPDATA\Microsoft\Signatures\woonmensen.htm"
 if (-not (Test-Path "$t")){
 	New-Item -ItemType Directory -Force -Path "$ENV:APPDATA\Microsoft\Signatures"
 	write-host "Nieuwe handtekening: $t"
 	copy "$s" "$t"
+}
+
+# Word Sjablonen verversen indien aangepast:
+$s = "$basedir\WordSjablonen\Startup\wm_gen.dotm"
+if (test-path "$s"){
+    # Source path bestaat, we bevinden ons in ons domein, copy actie starten:
+    $t = "$ENV:APPDATA\Microsoft\Word\Startup\wm_gen.dotm"
+    xcopy "$s" "$t" /D /Q /Y /R /C /K
+    
+    $s = "$basedir\WordSjablonen\Sjablonen"
+    $t = "$ENV:USERPROFILE\Documents\Aangepaste Office-sjablonen"
+    xcopy "$s" "$t" /D /Q /Y /R /C /K /S /I
 }
 
 Stop-Transcript
